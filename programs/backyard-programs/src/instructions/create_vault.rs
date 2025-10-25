2af081537b49ccd9c7db537df1b7425fe24c9650
affatanr <serhieiev.vladyslav@gmail.com>
2025-10-25 14:31:18 +0000

    fix: remove protocol index

--- programs/backyard-programs/src/instructions/create_vault.rs
+++ programs/backyard-programs/src/instructions/create_vault.rs
@@ -2,7 +2,7 @@ use crate::{errors::ErrorCode, Vault, MASTER_WALLET};
 use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
-#[instruction(protocol_index: u8, vault_id: Pubkey)]
+#[instruction(vault_id: Pubkey)]
 pub struct CreateVault<'info> {
     #[account(
         mut,
@@ -14,7 +14,7 @@ pub struct CreateVault<'info> {
         init,
         payer = master,
         space = 8 + Vault::INIT_SPACE,
-        seeds = [b"vault", protocol_index.to_le_bytes().as_ref(), vault_id.as_ref()],
+        seeds = [b"vault", vault_id.as_ref()],
         bump,
     )]
     pub vault: Account<'info, Vault>,
@@ -22,10 +22,9 @@ pub struct CreateVault<'info> {
     pub system_program: Program<'info, System>,
 }
 
-pub fn create_vault(ctx: Context<CreateVault>, protocol_index: u8, vault_id: Pubkey) -> Result<()> {
+pub fn create_vault(ctx: Context<CreateVault>, vault_id: Pubkey) -> Result<()> {
     let vault = &mut ctx.accounts.vault;
 
-    vault.protocol_index = protocol_index;
     vault.vault_id = vault_id;
     vault.bump = ctx.bumps.vault;
     Ok(())
